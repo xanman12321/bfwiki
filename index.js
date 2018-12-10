34888d4a14296aece6d0e0a338b82c60d1db5573
xanman12321 <xanderess@gmail.com>
2018-12-10 16:02:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-#top {
-  background-color: lightgrey;
-  float: left;
-  width: 100%;
-}
-#side {
-  background-color: lightgrey;
-  height: 500px;
-  width: 50px;
-  display: inline-block;
-}
-#head {
-  text-align: center;
-}
-#page {
-  margin-left: 50px;
-  vertical-align: top;
-  display: inline-block;
+function home(){
+  window.location.href = "https://xanman12321.github.io/bfwiki";
 }
