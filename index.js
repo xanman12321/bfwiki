5497ab935a789a8569fe57f7d0f5cb32b180c517
xanman12321 <xanderess@gmail.com>
2018-12-10 16:02:02 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+#top {
+  background-color: lightgrey;
+  float: left;
+  width: 100%;
+}
+#side {
+  background-color: lightgrey;
+  height: 500px;
+  width: 50px;
+  display: inline-block;
+}
+#head {
+  text-align: center;
+}
+#page {
+  margin-left: 50px;
+  vertical-align: top;
+  display: inline-block;
+}
